da85e5c03ccce21f523b58f426aaa340fa915d6b
Shravan Vasishth <shravanvasishth@Shravans-MBP.fritz.box>
2021-07-24 17:50:17 +0200

    fixed data set

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Data from an experiment involving an agreement attraction design with Likert ratings and accuracy of response \insertCite{AnnaLphd;textual}{lingpsych}
 #'
-#' The dataset, from experiment 1 in chapter 4 of \insertCite{AnnaLphd;textual}{lingpsych}, contains the responses from 4,296 subjects who participated in an internet-based study that elicited Likert ratings and question-response accuracies for a single sentence they were shown. There were two levels of a factor called attractor (levels: match and mismatch) and two levels of another factor called interference (levels: interference and no interference). The full data-set and code, including preprocessing steps, are available from: https://osf.io/eawg5/.
+#' The data set, from experiment 1 in chapter 4 of \insertCite{AnnaLphd;textual}{lingpsych}, contains the responses from 4,296 subjects who participated in an internet-based study that elicited Likert ratings and question-response accuracies for a single sentence they were shown. There were two levels of a factor called attractor (levels: match and mismatch) and two levels of another factor called interference (levels: interference and no interference). The full data set and code, including preprocessing steps, are available from: https://osf.io/eawg5/.
 #'
 #' @format A data frame with 4,296 rows and 7 variables:
 #' \describe{
@@ -19,7 +19,7 @@
 
 #' Data from an experiment with F1 frequency in Hertz of vowels produced by male and females speakers of different languages \insertCite{johnson2011quantitative;textual}{lingpsych}
 #'
-#' The dataset is from \insertCite{johnson2011quantitative;textual}{lingpsych}, contains the F1 frequencies recorded from 19 male subjects and 19 female subjects who produced vowels in their native languages.
+#' The data set is from \insertCite{johnson2011quantitative;textual}{lingpsych}, contains the F1 frequencies recorded from 19 male subjects and 19 female subjects who produced vowels in their native languages.
 #'
 #' @format A data frame with 19 rows and 4 variables:
 #' \describe{
@@ -34,7 +34,7 @@
 
 #' Data from a self-paced reading experiment that records reading times in milliseconds of subject and object relative clauses in English. \insertCite{grodner;textual}{lingpsych}
 #'
-#' The dataset is from Experiment 1 in \insertCite{grodner;textual}{lingpsych}, and contains reading times from a self-paced reading experiment on subject and object relative clauses in English. There are 42 subjects who read 16 items in a Latin square design. The data are from the relative clause verb, which is the critical region of interest.
+#' The data set is from Experiment 1 in \insertCite{grodner;textual}{lingpsych}, and contains reading times from a self-paced reading experiment on subject and object relative clauses in English. There are 42 subjects who read 16 items in a Latin square design. The data are from the relative clause verb, which is the critical region of interest.
 #'
 #' @format A data frame with 672 rows and 4 variables:
 #' \describe{
@@ -49,7 +49,7 @@
 
 #' Data from a self-paced reading experiment that records reading times in milliseconds of subject and object relative clauses in English. \insertCite{grodner;textual}{lingpsych}
 #'
-#' The dataset is from Experiment 1 in \insertCite{grodner;textual}{lingpsych}, and contains reading times from a self-paced reading experiment on subject and object relative clauses in English. There are 42 subjects who read 16 items in a Latin square design. The data are for all the words in the two sentence types.
+#' The data set is from Experiment 1 in \insertCite{grodner;textual}{lingpsych}, and contains reading times from a self-paced reading experiment on subject and object relative clauses in English. There are 42 subjects who read 16 items in a Latin square design. The data are for all the words in the two sentence types.
 #'
 #' @format A data frame with 10331 rows and 10 variables:
 #' \describe{
@@ -66,7 +66,7 @@
 
 #' Data from a self-paced reading experiment that records reading times in milliseconds of subject and object relative clauses in Chinese. \insertCite{gibsonwu;textual}{lingpsych}
 #'
-#' The dataset is from an experiment reported in \insertCite{gibsonwu;textual}{lingpsych}, and contains reading times from a self-paced reading experiment on subject and object relative clauses in Chinese. There are 37 subjects who read 15 items in a Latin square design. The data are from the relative clause head noun, which is the critical region of interest.
+#' The data set is from an experiment reported in \insertCite{gibsonwu;textual}{lingpsych}, and contains reading times from a self-paced reading experiment on subject and object relative clauses in Chinese. There are 37 subjects who read 15 items in a Latin square design. The data are from the relative clause head noun, which is the critical region of interest.
 #'
 #' @format A data frame with 547 rows and 4 variables:
 #' \describe{
@@ -81,7 +81,7 @@
 
 #' Data from a self-paced reading experiment that records reading times in milliseconds of subject and object relative clauses in Chinese. \insertCite{VasishthetalPLoSOne2013;textual}{lingpsych}
 #'
-#' The dataset is from a replication attempt of experiment by \insertCite{gibsonwu;textual}{lingpsych}; the replication attempt was reported in \insertCite{VasishthetalPLoSOne2013;textual}{lingpsych}, and contains reading times from a self-paced reading experiment on subject and object relative clauses in Chinese. There are 40 subjects who read 15 items in a Latin square design. The data are from the relative clause head noun, which is the critical region of interest.
+#' The data set is from a replication attempt of experiment by \insertCite{gibsonwu;textual}{lingpsych}; the replication attempt was reported in \insertCite{VasishthetalPLoSOne2013;textual}{lingpsych}, and contains reading times from a self-paced reading experiment on subject and object relative clauses in Chinese. There are 40 subjects who read 15 items in a Latin square design. The data are from the relative clause head noun, which is the critical region of interest.
 #'
 #' @format A data frame with 595 rows and 6 variables:
 #' \describe{
@@ -98,7 +98,7 @@
 
 #' Data from an eyetracking reading experiment that records total reading times in milliseconds at an auxiliary verb in so-called low and high interference conditions. \insertCite{DillonEtAl2013;textual}{lingpsych}
 #'
-#' The dataset is from an eyetracking experiment by \insertCite{DillonEtAl2013;textual}{lingpsych}, and contains reading times (total reading times) from a design that orginally had eight conditions. There are 40 subjects who read 48 items in a Latin square design. The data shown here focus on two conditions from the experiment, high and low interference conditions in a subject-verb number agreement configuration in ungrammatical sentences.
+#' The data set is from an eyetracking experiment by \insertCite{DillonEtAl2013;textual}{lingpsych}, and contains reading times (total reading times) from a design that orginally had eight conditions. There are 40 subjects who read 48 items in a Latin square design. The data shown here focus on two conditions from the experiment, high and low interference conditions in a subject-verb number agreement configuration in ungrammatical sentences.
 #'
 #' @format A data frame with 2,855 rows and 5 variables:
 #' \describe{
@@ -115,7 +115,7 @@
 
 #' Data from a self-paced reading experiment that records log reading times in log milliseconds of grammatical and ungrammatical double center embeddings in English. \insertCite{VSLK08;textual}{lingpsych}
 #'
-#' The dataset is from a self-paced reading study by \insertCite{VSLK08;textual}{lingpsych}, and contains reading times from a design that had two conditions, grammatical and ungrammatical double center embeddings in English. There are 48 subjects who read 16 items in a Latin square design. The data shown here focus on log reading times at the noun phrase following the final verb in a double center embedding.
+#' The data set is from a self-paced reading study by \insertCite{VSLK08;textual}{lingpsych}, and contains reading times from a design that had two conditions, grammatical and ungrammatical double center embeddings in English. There are 48 subjects who read 16 items in a Latin square design. The data shown here focus on log reading times at the noun phrase following the final verb in a double center embedding.
 #'
 #' @format A data frame with 768 rows and 4 variables:
 #' \describe{
@@ -130,7 +130,7 @@
 
 #' Data from a self-paced reading experiment that records log reading times in log milliseconds of grammatical and ungrammatical double center embeddings in Dutch. \insertCite{FrankEtAl2015;textual}{lingpsych}
 #'
-#' The dataset is from a self-paced reading study by \insertCite{FrankEtAl2015;textual}{lingpsych}, and contains reading times from a design that had two conditions, grammatical and ungrammatical double center embeddings in Dutch. There are 24 subjects who read 16 items in a Latin square design. The data shown here focus on log reading times at the noun phrase following the final verb in a double center embedding.
+#' The data set is from a self-paced reading study by \insertCite{FrankEtAl2015;textual}{lingpsych}, and contains reading times from a design that had two conditions, grammatical and ungrammatical double center embeddings in Dutch. There are 24 subjects who read 16 items in a Latin square design. The data shown here focus on log reading times at the noun phrase following the final verb in a double center embedding.
 #'
 #' @format A data frame with 372 rows and 4 variables:
 #' \describe{
@@ -145,7 +145,7 @@
 
 #' Data from the Potsdam-Allahabad Hindi Corpus, an eyetracking reading experiment that records various reading measures for a newspaper corpus in Hindi. \insertCite{HusainVasishthNarayanan2015;textual}{lingpsych}
 #'
-#' The dataset is from an eyetracking study by \insertCite{HusainVasishthNarayanan2015;textual}{lingpsych}, and contains various reading measures for a newspaper corpus in Hindi. The data are from a larger data-set and contain 10 subjects who read 83 items. The reading measures shown are for each word in each sentence.
+#' The data set is from an eyetracking study by \insertCite{HusainVasishthNarayanan2015;textual}{lingpsych}, and contains various reading measures for a newspaper corpus in Hindi. The data are from a larger data set and contain 10 subjects who read 83 items. The reading measures shown are for each word in each sentence.
 #'
 #' @format A data frame with 27,066 rows and 33 variables:
 #' \describe{
@@ -188,7 +188,7 @@
 
 #' Data from the Potsdam-Allahabad Hindi Corpus, an eyetracking reading experiment that records various reading measures for a newspaper corpus in Hindi. \insertCite{HusainVasishthNarayanan2015;textual}{lingpsych}
 #'
-#' The dataset is from an eyetracking study by \insertCite{HusainVasishthNarayanan2015;textual}{lingpsych}, and contains various reading measures for a newspaper corpus in Hindi. The data are from 30 subjects who read 83 items. The reading measures shown are for each word in each sentence.
+#' The data set is from an eyetracking study by \insertCite{HusainVasishthNarayanan2015;textual}{lingpsych}, and contains various reading measures for a newspaper corpus in Hindi. The data are from 30 subjects who read 83 items. The reading measures shown are for each word in each sentence.
 #'
 #' @format A data frame with 79,943 rows and 33 variables:
 #' \describe{
@@ -231,7 +231,7 @@
 
 #' Model fit for an eyetracking reading experiment that records total reading times in milliseconds for a 2x2x2 design. \insertCite{DillonEtAl2013;textual}{lingpsych}
 #'
-#' The dataset is a model fit for an English eyetracking reading study by \insertCite{DillonEtAl2013;textual}{lingpsych}. There are 40 subjects who read 48 items in a Latin square design. The data shown here focus on total reading times at the critical region.
+#' The data set is a model fit for an English eyetracking reading study by \insertCite{DillonEtAl2013;textual}{lingpsych}. There are 40 subjects who read 48 items in a Latin square design. The data shown here focus on total reading times at the critical region.
 #'
 #' @format A brms model fit
 #' \describe{
@@ -651,7 +651,7 @@
 
 #' Data from a self-paced reading experiment that records reading times in milliseconds at the post-critical region. \insertCite{swets2008underspecification;textual}{bcogsci}
 #'
-#' The dataset is from a self-paced reading experiment by \insertCite{swets2008underspecification;textual}{bcogsci}, and contains reading times from a 3x3 design. 
+#' The data set is from a self-paced reading experiment by \insertCite{swets2008underspecification;textual}{bcogsci}, and contains reading times from a 3x3 design. 
 #' 
 #' @format A data frame with 5,184 rows and 6 variables:
 #' \describe{
